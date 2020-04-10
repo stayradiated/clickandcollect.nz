6b501a7b50e841130572e5eb439ca486846828c3
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-04-10 20:54:16 +1200

    feat: add ios & android icons

--- src/app.tsx
+++ src/app.tsx
@@ -43,6 +43,12 @@ const App = () => {
       <Head>
         <title>{title}</title>
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
