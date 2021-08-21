162140a1216e2707fd30ff59a789f30addf9d028
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-08-21 22:19:25 +1200

    wip @ 08/21/21 22:19:25

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const API_ENDPOINT = 'https://api.clickandcollect.nz'
+// export const API_ENDPOINT = 'http://localhost:8000'
 
 export const CLOUD_ENDPOINT = 'https://cloud.clickandcollect.nz'
 // export const CLOUD_ENDPOINT = 'http://localhost:4000'
