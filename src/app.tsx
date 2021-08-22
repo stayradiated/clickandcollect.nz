ee8e9ffc61ff2dd6d0ce27364da69d6612faa244
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-08-22 19:27:55 +1200

    feat: remove email notifications link

--- src/app.tsx
+++ src/app.tsx
@@ -65,18 +65,6 @@ const App = () => {
       <main className={classNames({ selected: supermarket != null })}>
         {supermarket && <SupermarketInfo supermarket={supermarket} />}
         <footer>
-          <Link href="/subscribe-to-product-updates" passHref>
-            <a>
-              <EntypoBell
-                style={{
-                  width: '1.2em',
-                  height: '1.2em',
-                  marginRight: '0.5em',
-                }}
-              />
-              Email Notifications
-            </a>
-          </Link>
           <a
             target="_blank"
             href="https://contact.george.czabania.com/?product=clickandcollect.nz"
